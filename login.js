5256dd7b26ef60256b11016ec0ddbbf1cb869cca
mrp-v2 <matthrewp@gmail.com>
2024-01-31 08:17:54 -0700

    made login page accept non-empty username and password

---
+++ login.js
@@ -0,0 +1,13 @@
+function login() {
+    const username = document.querySelector("#username");
+    if (username.value === "") {
+        return;
+    }
+    const password = document.querySelector("#password");
+    if (password.value === "") {
+        return;
+    }
+    localStorage.setItem("username", username.value);
+    localStorage.setItem("password", password.value);
+    window.location.href = "saved.html";
+}
