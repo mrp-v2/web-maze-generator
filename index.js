3d962e06e12621a16a48a94fb16c3d7cb1764dd5
mrp-v2 <matthrewp@gmail.com>
2024-02-21 07:30:24 -0700

    started making index.js

--- index.js
+++ index.js
@@ -1,69 +1,29 @@
-import { Maze } from './modules/maze.js';
+const express = require('express');
+const app = express();
 
-function generate_maze_clicked() {
-    /** @type {number} */
-    let width = document.querySelector("#width-input").value;
-    if (width < 5){
-        width = 5;
-    }
-    else if (width > 50){
-        width = 50;
-    }
-    /** @type {number} */
-    let height = document.querySelector("#height-input").value;
-    if (height < 5){
-        height = 5;
-    }
-    else if (height > 50){
-        height = 50;
-    }
-    const maze = new Maze(width, height);
-    maze.draw_maze(document.querySelector("#generated-maze-canvas"));
-    sessionStorage.setItem("current_maze", maze.to_string());
-}
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
-function save_maze_clicked() {
-    let current_maze = sessionStorage.getItem("current_maze");
-    if (current_maze !== null) {
-        let saved_mazes = sessionStorage.getItem("saved_mazes");
-        if (saved_mazes !== null) {
-            /** @type {Array<string>} */
-            let mazes = JSON.parse(saved_mazes);
-            mazes.push(current_maze)
-            sessionStorage.setItem("saved_mazes", JSON.stringify(mazes));
-        } else {
-            /** @type {Array<Maze>} */
-            let mazes = new Array();
-            mazes.push(current_maze);
-            sessionStorage.setItem("saved_mazes", JSON.stringify(mazes));
-        }
-    }
-    update_latest_saved_mazes();
-}
+app.use(express.json());
+app.use(express.static('public'));
 
-function update_latest_saved_mazes(){
-    let saved_mazes = sessionStorage.getItem("saved_mazes");
-    if (saved_mazes !== null){
-        /** @type {Array<string>} */
-        let mazes = JSON.parse(saved_mazes);
-        let count = mazes.length;
-        if (count >= 1){
-            let svg = document.querySelector("#latest-saved-maze-1");
-            let maze = Maze.from_string(mazes[count - 1]);
-            maze.draw_maze(svg);
-        }
-        if (count >= 2){
-            let svg = document.querySelector("#latest-saved-maze-2");
-            let maze = Maze.from_string(mazes[count - 2]);
-            maze.draw_maze(svg);
-        }
-        if (count >= 3){
-            let svg = document.querySelector("#latest-saved-maze-3");
-            let maze = Maze.from_string(mazes[count - 3]);
-            maze.draw_maze(svg);
-        }
-    }
-}
+app.use((req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
 
-document.querySelector("#generate-button").addEventListener('click', generate_maze_clicked);
-document.querySelector("#save-button").addEventListener('click', save_maze_clicked);
+const apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+apiRouter.get('/saved_mazes', (req, res) => {
+    // TODO
+});
+
+apiRouter.post('/saved_maze', (req, res) => {
+    // TODO
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
+
+let saved_mazes = {};
+
