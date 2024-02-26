68c2c7d7cfbab0f05067d43e6f342c6ef1fc09d5
mrp-v2 <matthrewp@gmail.com>
2024-02-26 07:22:02 -0700

    implemented temporary login button functionality

--- public/login.js
+++ public/login.js
@@ -1,3 +1,13 @@
 function login_clicked() {
-    
+    const username = document.querySelector("#username");
+    if (username.value === "") {
+        return;
+    }
+    const password = document.querySelector("#password");
+    if (password.value === "") {
+        return;
+    }
+    sessionStorage.setItem("username", username.value);
+    sessionStorage.setItem("password", password.value);
+    window.location.href = "saved.html";
 }
