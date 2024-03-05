77ea50bc18a90c30904b3699c3d33cd555ba84ef
mrp-v2 <matthrewp@gmail.com>
2024-03-05 08:50:44 -0700

    fixed saved mazes svgs not rendering

--- public/saved.js
+++ public/saved.js
@@ -1,4 +1,4 @@
-import {Maze} from "./modules/maze.js";
+import {Maze} from './modules/maze.js';
 
 /**
  * 
@@ -6,8 +6,8 @@ import {Maze} from "./modules/maze.js";
  */
 async function delete_maze_pressed(event) {
     /** @type {string} */
-    let target = event.target.id;
-    let index = Number(target.substring(11, target.length - 7));
+    const target = event.target.id;
+    const index = Number(target.substring(11, target.length - 7));
     /** @type { Array } */
     const mazes = JSON.parse(localStorage.getItem('saved_mazes'))
 
@@ -25,46 +25,46 @@ async function delete_maze_pressed(event) {
  * @param {string} id 
  */
 function add_maze(maze, id){
-    let main = document.querySelector("main");
-    let maze_div = document.createElement("div");
-    maze_div.setAttribute("id", id)
-    let svg = document.createElement("svg");
-    let info_div = document.createElement("div");
-    let size_div = document.createElement("div");
-    size_div.setAttribute("class", "text-div");
-    size_div.textContent = `${maze.width()} x ${maze.height()}`;
-    let button_div = document.createElement("div");
-    button_div.setAttribute("class", "button-div");
-    let delete_button = document.createElement("button");
-    delete_button.setAttribute("type", "button");
-    delete_button.addEventListener("click", delete_maze_pressed);
-    delete_button.setAttribute("id", `${id}-delete`)
-    delete_button.textContent = "Delete";
-    button_div.appendChild(delete_button);
-    info_div.appendChild(size_div);
-    info_div.appendChild(button_div);
-    maze_div.appendChild(svg);
-    maze_div.appendChild(info_div);
-    maze.draw_maze(svg);
+    const main = document.querySelector('main');
+    const maze_div = document.createElement('div');
     main.appendChild(maze_div);
+    maze_div.setAttribute('id', id)
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    maze_div.appendChild(svg);
+    const info_div = document.createElement('div');
+    maze_div.appendChild(info_div);
+    const size_div = document.createElement('div');
+    info_div.appendChild(size_div);
+    size_div.setAttribute('class', 'text-div');
+    size_div.textContent = `${maze.width()} x ${maze.height()}`;
+    const button_div = document.createElement('div');
+    info_div.appendChild(button_div);
+    button_div.setAttribute('class', 'button-div');
+    const delete_button = document.createElement('button');
+    button_div.appendChild(delete_button);
+    delete_button.setAttribute('type', 'button');
+    delete_button.addEventListener('click', delete_maze_pressed);
+    delete_button.setAttribute('id', `${id}-delete`)
+    delete_button.textContent = 'Delete';
+    maze.draw_maze(svg);
 }
 
 async function update_mazes() {
-    let main = document.querySelector("main");
+    const main = document.querySelector('main');
     while (main.firstChild){
         main.removeChild(main.firstChild);
     }
     /** @type {Array<string>} */
-    let saved_mazes = JSON.parse(localStorage.getItem("saved_mazes"));
+    let saved_mazes = JSON.parse(localStorage.getItem('saved_mazes'));
     const response = await fetch(`/api/mazes/${localStorage.getItem('username')}`, {
         method: 'GET'
     });
     if (response.ok){
         saved_mazes = await response.json();
-        localStorage.setItem("saved_mazes", JSON.stringify(saved_mazes));
+        localStorage.setItem('saved_mazes', JSON.stringify(saved_mazes));
     }
     if (saved_mazes !== null){
-        let mazes = saved_mazes;
+        const mazes = saved_mazes;
         for (let i in saved_mazes){
             add_maze(Maze.from_json(mazes[i]), `saved-maze-${i}`);
         }
