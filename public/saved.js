7491b360c616fb24ca8c2b57b159437664701825
mrp-v2 <matthrewp@gmail.com>
2024-03-14 08:29:06 -0600

    simplified maze deletion button action

--- public/saved.js
+++ public/saved.js
@@ -2,33 +2,13 @@ import {Maze} from './modules/maze.js';
 
 /**
  * 
- * @param {PointerEvent} event 
- */
-async function delete_maze_pressed(event) {
-    /** @type {string} */
-    const target = event.target.id;
-    const index = Number(target.substring(11, target.length - 7));
-    /** @type { Array } */
-    const mazes = JSON.parse(localStorage.getItem('saved_mazes'))
-
-    await fetch('/api/delete_maze', {
-        method: 'POST',
-        headers: {'content-type': 'application/json; charset=UTF-8'},
-        body: JSON.stringify(mazes[index])
-    })
-    await update_mazes();
-}
-
-/**
- * 
  * @param {Maze} maze 
- * @param {string} id 
+ * @param {Number} index 
  */
-function add_maze(maze, id){
+function add_maze(maze, index){
     const main = document.querySelector('main');
     const maze_div = document.createElement('div');
     main.appendChild(maze_div);
-    maze_div.setAttribute('id', id)
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     maze_div.appendChild(svg);
     const info_div = document.createElement('div');
@@ -43,8 +23,14 @@ function add_maze(maze, id){
     const delete_button = document.createElement('button');
     button_div.appendChild(delete_button);
     delete_button.setAttribute('type', 'button');
-    delete_button.addEventListener('click', delete_maze_pressed);
-    delete_button.setAttribute('id', `${id}-delete`)
+    delete_button.addEventListener('click', async () => {
+        await fetch('/api/delete_maze', {
+            method: 'POST',
+            headers: {'content-type': 'application/json; charset=UTF-8'},
+            body: JSON.stringify({ index: index })
+        });
+        await update_mazes();
+    });
     delete_button.textContent = 'Delete';
     maze.draw_maze(svg);
 }
@@ -54,14 +40,13 @@ async function update_mazes() {
     while (main.firstChild){
         main.removeChild(main.firstChild);
     }
-    /** @type {Array<string>} */
     const response = await fetch(`/api/mazes/${localStorage.getItem('username')}`, {
         method: 'GET'
     });
     if (response.ok){
         const saved_mazes = await response.json();
         for (let i in saved_mazes) {
-            add_maze(Maze.from_json(saved_mazes[i]), `saved-maze-${i}`);
+            add_maze(Maze.from_json(saved_mazes[i]), Number(i));
         }
     }
 }
